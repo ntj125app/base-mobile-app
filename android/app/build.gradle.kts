e5236500a6b2b4c06353415237f34c6f71b910a8
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-03-20 21:59:36 +0700

    Removed key password

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
         create("release") {
             try {
                 keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
                 storeFile = keystoreProperties["storeFile"]?.let { file(it) }
                 storePassword = keystoreProperties["storePassword"] as String
             } catch (e: Exception) {
